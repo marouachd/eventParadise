ba6824e5a6f6286eb958f34653384771fd2570db
maroua <chroud.maroua@gmail.com>
2022-12-12 11:10:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const form = document.querySelector("form"); //recupération de l'ensemble par form (tout les elements)
+const elements = form.elements; // constante elements qui conserne les inputs 
+
+for (const element of elements) { // for(String str: strings) // boucle
+    const type = element.type;
+    const id = element.id;
+    if (type != "submit") {
+        element.addEventListener("invalid", (event) => { // chaque itération il ajoute un ecouteur de type 'invalide' on peut mettre type click
+            const helpText = document.getElementById(`${id}`);
+
+            element.classList.add("is-invalid");
+            helpText.classList.add("text-danger");
+
+
+        });
+
+
+
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            console.log("implement form reset");
+            console.log("implement toaster");
+        });
+    }
+}
