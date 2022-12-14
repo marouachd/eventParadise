c052d6b572bcd93352e5e74365a491a53edf1372
maroua <chroud.maroua@gmail.com>
2022-12-14 10:28:55 +0100

    validation date OK

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 
+var d = new Date();
+var day = d.getDate();
+var month = d.getMonth() + 1;
+var year = d.getFullYear();
+if (day < 10) {
+    day = "0" + day;
+}
+if (month < 10) {
+    month = "0" + month;
+}
+var tdate = year + "-" + month + "-" + day;
+
+const date_min = tdate;
+
+const date = document.querySelector('#date')
+date.setAttribute("min", date_min)
+
 const form = document.querySelector("form"); //recupération de l'ensemble par form (tout les elements)
 const elements = form.elements; // constante elements qui conserne les inputs 
 
@@ -9,7 +26,7 @@ for (const element of elements) { // for(String str: strings) // boucle
     const id = element.id;
     const helpText = document.getElementById(`help${id}`);
     const select = document.querySelector("select");
-    const date = document.getElementById("date");
+
     if (type != "submit") {
         element.addEventListener("invalid", (event) => { // chaque itération il ajoute un ecouteur de type 'invalide' on peut mettre type click&
 
@@ -25,9 +42,8 @@ for (const element of elements) { // for(String str: strings) // boucle
 for (const element of elements) {
     const id = element.id;
     const helpText = document.getElementById(`help${id}`);
-    const select = document.querySelector("select");
     const date = document.getElementById("date");
-    element.addEventListener("input", (event) => {
+    element.addEventListener("change", (event) => {
         if (element.validity.valid) {
             element.classList.remove("is-invalid");
             element.classList.add("is-valid");
@@ -35,6 +51,7 @@ for (const element of elements) {
             helpText.classList.add("text-success");
 
         } else {
+
             element.classList.remove("is-valid");
             element.classList.add("is-invalid");
             helpText.classList.remove("text-success");
@@ -42,25 +59,8 @@ for (const element of elements) {
         }
     });
 
-    element.addEventListener("change", (event) => {
-        if (element.validity.valid) {
-            date.classList.remove("is-invalid");
-            date.classList.add("is-valid");
-            select.classList.remove("is-invalid");
-            select.classList.add("is-valid");
-            helpText.classList.remove("text-danger");
-            helpText.classList.add("text-success");
-        } else {
-
-            date.classList.remove("is-valid");
-            date.classList.add("is-invalid");
-            select.classList.remove("is-valid");
-            select.classList.add("is-invalid");
-            helpText.classList.remove("text-success");
-            helpText.classList.add("text-danger");
-        }
-    });
 }
+
 
 
 
